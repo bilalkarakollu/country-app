c04aded25bf296a7739bb7d5d7d713ac763fae8e
bilalkarakollu <karakollubilal@gmail.com>
2022-04-17 14:09:17 +0300

    types created

--- src/types/APICountyType.ts
+++ src/types/APICountyType.ts
@@ -1,61 +1,253 @@
-export interface Currency {
-    code: string;
+export interface Spa {
+    official: string;
+    common: string;
+}
+
+export interface NativeName {
+    spa: Spa;
+}
+
+export interface Name {
+    common: string;
+    official: string;
+    nativeName: NativeName;
+}
+
+export interface UYU {
     name: string;
     symbol: string;
 }
 
-export interface Language {
-    iso639_1: string;
-    iso639_2: string;
-    name: string;
-    nativeName: string;
+export interface Currencies {
+    UYU: UYU;
+}
+
+export interface Idd {
+    root: string;
+    suffixes: string[];
+}
+
+export interface Languages {
+    spa: string;
+}
+
+export interface Ara {
+    official: string;
+    common: string;
+}
+
+export interface Ces {
+    official: string;
+    common: string;
+}
+
+export interface Cym {
+    official: string;
+    common: string;
+}
+
+export interface Deu {
+    official: string;
+    common: string;
+}
+
+export interface Est {
+    official: string;
+    common: string;
+}
+
+export interface Fin {
+    official: string;
+    common: string;
+}
+
+export interface Fra {
+    official: string;
+    common: string;
+}
+
+export interface Hrv {
+    official: string;
+    common: string;
+}
+
+export interface Hun {
+    official: string;
+    common: string;
+}
+
+export interface Ita {
+    official: string;
+    common: string;
+}
+
+export interface Jpn {
+    official: string;
+    common: string;
+}
+
+export interface Kor {
+    official: string;
+    common: string;
+}
+
+export interface Nld {
+    official: string;
+    common: string;
+}
+
+export interface Per {
+    official: string;
+    common: string;
+}
+
+export interface Pol {
+    official: string;
+    common: string;
+}
+
+export interface Por {
+    official: string;
+    common: string;
+}
+
+export interface Rus {
+    official: string;
+    common: string;
+}
+
+export interface Slk {
+    official: string;
+    common: string;
+}
+
+export interface Spa2 {
+    official: string;
+    common: string;
+}
+
+export interface Swe {
+    official: string;
+    common: string;
+}
+
+export interface Urd {
+    official: string;
+    common: string;
+}
+
+export interface Zho {
+    official: string;
+    common: string;
 }
 
 export interface Translations {
-    br: string;
-    pt: string;
-    nl: string;
-    hr: string;
-    fa: string;
-    de: string;
-    es: string;
-    fr: string;
-    ja: string;
-    it: string;
-    hu: string;
+    ara: Ara;
+    ces: Ces;
+    cym: Cym;
+    deu: Deu;
+    est: Est;
+    fin: Fin;
+    fra: Fra;
+    hrv: Hrv;
+    hun: Hun;
+    ita: Ita;
+    jpn: Jpn;
+    kor: Kor;
+    nld: Nld;
+    per: Per;
+    pol: Pol;
+    por: Por;
+    rus: Rus;
+    slk: Slk;
+    spa: Spa2;
+    swe: Swe;
+    urd: Urd;
+    zho: Zho;
 }
 
-export interface RegionalBloc {
-    acronym: string;
-    name: string;
-    otherNames: string[];
-    otherAcronyms: string[];
+export interface Eng {
+    f: string;
+    m: string;
+}
+
+export interface Fra2 {
+    f: string;
+    m: string;
+}
+
+export interface Demonyms {
+    eng: Eng;
+    fra: Fra2;
+}
+
+export interface Maps {
+    googleMaps: string;
+    openStreetMaps: string;
+}
+
+export interface Gini {
+    2019: number;
+}
+
+export interface Car {
+    signs: string[];
+    side: string;
+}
+
+export interface Flags {
+    png: string;
+    svg: string;
+}
+
+export interface CoatOfArms {
+    png: string;
+    svg: string;
+}
+
+export interface CapitalInfo {
+    latlng: number[];
+}
+
+export interface PostalCode {
+    format: string;
+    regex: string;
 }
 
 export interface CountryType {
-    name: string;
-    topLevelDomain: string[];
-    alpha2Code: string;
-    alpha3Code: string;
-    callingCodes: string[];
-    capital: string;
-    altSpellings: string[];
-    region: string;
-    continent: string;
-    population: number;
-    latlng: number[];
-    demonym: string;
-    area: number;
-    gini: number;
-    timezones: string[];
-    borders: string[];
-    nativeName: string;
-    numericCode: string;
-    currencies: Currency[];
-    languages: Language[];
-    translations: Translations;
-    flags: string[];
-    regionalBlocs: RegionalBloc[];
+    name: Name;
+    tld: string[];
+    cca2: string;
+    ccn3: string;
+    cca3: string;
     cioc: string;
     independent: boolean;
+    status: string;
+    unMember: boolean;
+    currencies: Currencies;
+    idd: Idd;
+    capital: string[];
+    altSpellings: string[];
+    region: string;
+    subregion: string;
+    languages: Languages;
+    translations: Translations;
+    latlng: number[];
+    landlocked: boolean;
+    borders: string[];
+    area: number;
+    demonyms: Demonyms;
+    flag: string;
+    maps: Maps;
+    population: number;
+    gini: Gini;
+    fifa: string;
+    car: Car;
+    timezones: string[];
+    continents: string[];
+    flags: Flags;
+    coatOfArms: CoatOfArms;
+    startOfWeek: string;
+    capitalInfo: CapitalInfo;
+    postalCode: PostalCode;
 }
